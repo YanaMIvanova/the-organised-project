b16cd90386daa00958c444fe014a2d30e070e0ae
Yana Ivanova <yana@plam.ch>
2022-02-10 12:46:13 +0200

    use auth0 for login and logout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
 import './App.scss'
 import Dashboard from '../Dashboard/Dashboard'
 import Preferences from '../Preferences/Preferences'
@@ -8,14 +9,22 @@ import { useToken } from './hooks'
 
 function App() {
     const { token, setToken } = useToken()
+    const { loginWithRedirect, isAuthenticated, logout, isLoading } = useAuth0()
 
-    if (!token) {
-        return <Login setToken={setToken} />
+    if (isLoading) {
+        return <h3>Loading...</h3>
+    }
+
+    if (!isAuthenticated) {
+        /* <Login setToken={setToken} /> */
+
+        return <button onClick={() => loginWithRedirect()}>Log In</button>
     }
 
     return (
         <div className="wrapper">
             <h1>Application</h1>
+            <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>
             <BrowserRouter>
                 <Routes>
                     <Route path="/dashboard" element={<Dashboard />} />
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,20 @@ import './index.scss'
 import App from './components/App/App'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import { Auth0Provider } from '@auth0/auth0-react'
 import * as serviceWorker from './serviceWorker'
 
+const DOMAIN = 'organised-project.eu.auth0.com'
+const CLIENT_ID = 'F6eBlvCl5ACGnssGO2cuwXUFlFkplkDd'
+
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
+    <Auth0Provider domain={DOMAIN} clientId={CLIENT_ID} redirectUri={window.location.origin}>
+        <React.StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </React.StrictMode>
+    </Auth0Provider>,
     document.getElementById('root')
 )
 
